58613e66513f86fd32e4541aca7d200444770d94
slopate <31645781+slopate@users.noreply.github.com>
2019-10-02 01:19:33 -0400

    Update buttonTest.ino

--- buttonTest.ino
+++ buttonTest.ino
@@ -1,37 +1,85 @@
+//#include <SimpleMessageSystem.h>
 
-int buttonPin = 21;
-int switchPin = 22;
-int joystickX = 35;
-int joystickY = 34;
-int buttonVal = 0;
-int switchVal = 0;
-int stickValX = 0;
-int stickValY = 0;
+
+int buttonPinR = 27;
+int buttonPinL = 14;
+int buttonValR = 0;
+int buttonValL = 0;
+
+
+int joystickXR = 34;
+int joystickYR = 35;
+int joystickXL = 32;
+int joystickYL = 33;
+int stickValXR = 0;
+int stickValYR = 0;
+int stickValXL = 0;
+int stickValYL = 0;
+
+
+int switchPinR = 25;
+int switchPinL = 13;
+int switchValR = 0;
+int switchValL = 0;
+
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(buttonPin, INPUT_PULLUP);
-  pinMode(switchPin, INPUT_PULLUP);
+  pinMode(buttonPinR, INPUT_PULLUP);
+  pinMode(buttonPinL, INPUT_PULLUP);
+  pinMode(switchPinR, INPUT_PULLUP);
+  pinMode(switchPinL, INPUT_PULLUP);
 
-  pinMode(joystickX, INPUT);
-  pinMode(joystickY, INPUT);
- //digitalWrite(switchPin, HIGH);
+  pinMode(joystickXR, INPUT);
+  pinMode(joystickXL, INPUT);
+  pinMode(joystickYR, INPUT);
+  pinMode(joystickYL, INPUT);
+  
   Serial.begin(9600);
-
- // digitalWrite(joystickX, HIGH);
+  //delay(1000);
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  buttonVal = digitalRead(buttonPin);
- // switchVal = digitalRead(switchPin);
+  buttonValR = digitalRead(buttonPinR);
+  buttonValL = digitalRead(buttonPinL);
+  
+  switchValR = digitalRead(switchPinR);
+  switchValL = digitalRead(switchPinL);
   //Serial.println(val);
   //Serial.println(buttonVal);
 
-  stickValX = analogRead(joystickX);
-  delay(100);
-  stickValY = analogRead(joystickY);
+  if(digitalRead(buttonPinR) == LOW){
+    delay(10);
+    if(digitalRead(buttonPinR) == LOW){
+      buttonValR = 1;
+    }
+    else{
+      buttonValR = 0;
+    }
+  }
+  else{
+    buttonValR = 0;
+  }
 
-  Serial.println(String(stickValX) + " " + String(stickValY));
+  if(digitalRead(buttonPinL) == LOW){
+    delay(10);
+    if(digitalRead(buttonPinL) == LOW){
+      buttonValL = 1;
+    }
+    else{
+      buttonValL = 0;
+    }
+  }
+  else{
+    buttonValL = 0;
+  }
+  
+  stickValXR = analogRead(joystickXR);
+  stickValXL = analogRead(joystickXL);
+  stickValYR = analogRead(joystickYR);
+  stickValYL = analogRead(joystickYL);
+
+  Serial.println(String(stickValXR) + " " + String(stickValYR) + " " + String(buttonValR) + " " + String(switchValR) + " " + String(stickValXL) + " " + String(stickValYL) + " " + String(buttonValL) + " " + String(switchValL));
 }
